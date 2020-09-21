ee4a7a96ad24a172d0138a90d42a7f3ceaa19a15
Jeremy Herve <jeremy@tagada.hu>
2020-09-21 06:32:55 +0000

    Only show featured images on post pages

--- content.php
+++ content.php
@@ -19,7 +19,7 @@
 			<?php if (
 				has_post_thumbnail()
 				&& in_array( get_post_format(), array( 'gallery', false ) )
-				&& ! is_post_type_archive( 'traktivity_event' )
+				&& 'post' === get_post_type()
 			) : ?>
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'independent-publisher' ), the_title_attribute( 'echo=0' ) ) ); ?>"><?php the_post_thumbnail( 'independent_publisher_post_thumbnail' ); ?></a>
 			<?php endif; ?>
